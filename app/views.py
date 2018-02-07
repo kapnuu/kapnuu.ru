a8ff6ed46275d92f5551b36ed92fa5f4c855d6c1
Alexander Pryanichnikov <pryanichnikov.av@gmail.com>
2018-02-07 16:37:15 +0300

    add try/except

--- app/views.py
+++ app/views.py
@@ -4,8 +4,12 @@ from flask import request
 @app.route('/')
 @app.route('/index')
 def index():
-    html = '<h3>kapnuu.ru</h3><pre>'
-    for h in request.headers:
-        html += h + ': ' + request.headers[h] + '\n'
-    html += '</pre>this will be <strong>kapnuu</strong> personal site'
+    html = '<h3>kapnuu.ru</h3>'
+    try:
+        html += '<pre>'
+        for h in request.headers:
+            html += h + ': ' + request.headers[h] + '\n'
+        html += '</pre>this will be <strong>kapnuu</strong> personal site'
+    except Exception as ex:
+        print('%s' % ex)
     return html

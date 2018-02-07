2e02a6910b25f1ba1212f11c365ca40a381df045
Alexander Pryanichnikov <pryanichnikov.av@gmail.com>
2018-02-07 16:27:49 +0300

    dump http headers

--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import request
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    html = '<h3>kapnuu.ru</h3><pre>'
+    for h in request.headers:
+        html += h + ': ' + request.headers[h] + '\n'
+    html += '</pre>this will be <strong>kapnuu</strong> personal site'
+    return html

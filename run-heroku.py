8156d0e0091f7039d6c6f275774f7c819eed5ca9
Alexander Pryanichnikov <pryanichnikov.av@gmail.com>
2018-02-07 16:16:57 +0300

    correct app start

--- run-heroku.py
+++ run-heroku.py
@@ -1,3 +1,7 @@
 #!venv/Scripts/python
+import os
 from app import app
-app.run(debug = True)
+
+port = int(os.environ.get('PORT', 5000))
+app.secret_key = os.urandom(16)
+app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=port)

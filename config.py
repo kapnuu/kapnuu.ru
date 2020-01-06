94c222ba3b2234a337f436fbe6be8ce9f4f9e180
kapnuu <pryanichnikov.av@gmail.com>
2020-01-06 03:05:01 +0300

    update config.py for heroku

--- config.py
+++ config.py
@@ -5,7 +5,14 @@ __author__ = 'pav'
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
-SECRET_KEY = 'everybody-love-cats:)'
+# SECRET_KEY = 'everybody-love-cats:)'
+SECRET_KEY = os.urandom(16)
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+database_uri = os.environ.get('DATABASE_URL')
+if database_uri is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = database_uri
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
